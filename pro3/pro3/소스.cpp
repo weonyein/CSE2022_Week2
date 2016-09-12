0c43a2c557db8348e799456598da5e07b8217379
wonyein <yeah1295@naver.com>
2016-09-13 01:31:52 +0900

    dd

---
+++ pro3/pro3/소스.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+double getrandom(const double min, const double max)
+{
+	double temp = (double)rand() / (double)RAND_MAX;
+	temp = min + (max - min)*temp;
+	return temp;
+}
+
+bool circle1(const double x, const double y)
+{
+	const double x_c = 0.5;
+	const double y_c = 0.5;
+	const double r = 0.5;
+
+	const double f = (x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) - r*r;
+
+	if (f > 0.0) return false;
+	else return true;
+}
+
+bool circle2(const double x, const double y)
+{
+	const double x_c = 3.5;
+	const double y_c = 0.5;
+	const double r = 0.5;
+
+	const double f = (x - x_c)*(x - x_c) + (y - y_c)*(y - y_c) - r*r;
+
+	if (f > 0.0) return false;
+	else return true;
+}
+
+void main()
+{
+	FILE *of = fopen("dumbel.text", "w");
+
+	srand((unsigned int)time(NULL));
+
+	for (int i = 0; i < 10000; i++)
+	{
+		double x = getrandom(0.0, 1.0);
+		double y = getrandom(0.0, 1.0);
+
+		if (circle1(x, y) == true)
+			fprintf(of, "%f %f\n", x, y);
+	}
+
+	for (int i = 0; i < 10000; i++)
+	{
+		double x = getrandom(1.0, 3.0);
+		double y = getrandom(0.25, 0.75);
+
+		fprintf(of, "%f %f\n", x, y);
+	}
+
+	for (int i = 0; i < 10000; i++)
+	{
+		double x = getrandom(3.0, 4.0);
+		double y = getrandom(0.0, 1.0);
+
+		if (circle2(x, y) == true)
+			fprintf(of, "%f %f\n", x, y);
+	}
+	fclose(of);
+}
